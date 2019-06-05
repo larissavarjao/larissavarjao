252dd6d60246949a07caa68b9faa27d6e7d85eab
Larissa <larissa.varjao@cubos.io>
2019-06-05 00:46:28 -0300

    feat(cover): adding new background

--- src/components/cover/index.tsx
+++ src/components/cover/index.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 import "./style.less";
+import { GitHub } from "../svgs/github";
+import { Facebook } from "../svgs/facebook";
+import { Linkedin } from "../svgs/linkedin";
+import { Link } from "gatsby";
 
 const Cover = () => (
     <>
-        <div className="cover"></div>
+        <div className="cover">
+            <div className="cover-card">
+                <h1>Fullstack Web Developer</h1>
+                <div className="social-media">
+                    <a href="https://www.linkedin.com/in/larissa-varj%C3%A3o-152932b8/" target="_blank">
+                        <Linkedin />
+                    </a>
+                    <a href="https://www.facebook.com/larivarjao" target="_blank">
+                        <Facebook />
+                    </a>
+                    <a href="https://github.com/larissavarjao" target="_blank">
+                        <GitHub />
+                    </a>
+                </div>
+            </div>
+        </div>
 
         <div className="white-parallax">
         </div>
