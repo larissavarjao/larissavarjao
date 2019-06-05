f3c8c3a657b80ea667cbdb06eb1077c8e8dd09c7
Larissa <larissa.varjao@cubos.io>
2019-06-05 11:40:12 -0300

    feat(cover): final cover!(hoppefully)

--- src/components/cover/index.tsx
+++ src/components/cover/index.tsx
@@ -1,24 +1,14 @@
 import * as React from "react";
 import "./style.less";
-import { GitHub } from "../svgs/github";
-import { Facebook } from "../svgs/facebook";
-import { Linkedin } from "../svgs/linkedin";
+
+const developer = require("../../images/developer.png");
 
 const Cover = () => (
     <div className="cover">
-        <div className="cover-card">
-            <h1><span className="title-bold">Fullstack</span> Web Developer</h1>
-            <div className="social-media">
-                <a href="https://www.linkedin.com/in/larissa-varj%C3%A3o-152932b8/" target="_blank">
-                    <Linkedin />
-                </a>
-                <a href="https://www.facebook.com/larivarjao" target="_blank">
-                    <Facebook />
-                </a>
-                <a href="https://github.com/larissavarjao" target="_blank">
-                    <GitHub />
-                </a>
-            </div>
+        <h1>Front End & Back End Web Developer</h1>
+        <h3>Desenho e programo sites, e eu <span>amo</span> o que eu faço.</h3>
+        <div className="social-media">
+            <img src={developer} className="developer" />
         </div>
     </div>
 )
---
+++ src/components/coverparallax/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import "./style.less";
+
+const CoverParallax = () => (
+    <div className="cover-parallax">
+    </div>
+)
+
+export default CoverParallax;
