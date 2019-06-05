459850ae9a99c7cba445a849bfe18ccb6b09405f
Larissa <larissa.varjao@cubos.io>
2019-06-04 22:59:25 -0300

    feat(cover): adding style and transform to less

---
+++ src/components/cover/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.less";
+
+const Cover = () => (
+    <div className="cover">
+        <h1>Em construção...</h1>
+    </div>
+)
+
+export default Cover;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import "./style.less";
+import SEO from "../components/seo";
+
+import 'antd/dist/antd';
+import "../styles/index.less";
 import "../styles/global.less";
 
 const IndexPage = () => (
   <div className="container">
+    <SEO title="Larissa Varjão" />
     <h1>Em construção...</h1>
   </div>
 )
